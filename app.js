4693de7fff91a40292791f1b65ce52f19b6f9c24
unknown <andrewtocchi@gmail.com>
2020-12-02 20:27:12 -0800

    added test.js for testing transport connections to SMTP

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 const SMTPServer = require("smtp-server").SMTPServer;
 const port = 25;
+// const host = '192.168.50.57';
 
 const server = new SMTPServer({
+    name: 'mail.nudegoals.com',
+    secure: false,
+    logger: true,
     onMailFrom(address, session, callback) {
       console.log(address.address)
+      return callback();
     },
     onAuth(auth, session, callback) {
       if (auth.username !== "abc" || auth.password !== "def") {
-        return callback(new Error("Invalid username or password"));
+        return callback(new Error("Invalid username or password?>?"));
       }
       console.log(session.envelope.mailFrom);
-      callback(null, { user: 123 }); // where 123 is the user id or similar property
+      callback(null, { user: "CEO" }); // where 123 is the user id or similar property
     },
-    onData(stream, session, callback) {
-      console.log(session.envelope.mailFrom);
-    }
+     onData(stream, session, callback) {
+    stream.pipe(process.stdout); // print message to console
+    stream.on("end", callback);
+    return callback();
+  }
  });
-
+console.log
 server.listen(port, console.log('started'))
+
+
 
 server.on("error", err => {
     console.log("Error %s", err.message);
---
+++ test.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer');
+
+var transport = nodemailer.createTransport({
+    host: "mail.nudegoals.com",
+    secure: false,
+    port: 25,
+    auth: { 
+      user: "abc",
+      pass: "def"
+    },
+    debug: true,
+    logger: true,
+    tls: {
+        rejectUnauthorized: false
+      }
+  });
+
+  var mailOptions = {
+    from: '"CEO Nude" <CEO@nudegoals.com>',
+    to: 'andrewtocchi@gmail.com',
+    subject: 'Nice Nodemailer test',
+    text: 'Hey there, it’s our first message sent with Nodemailer ;) ', 
+    html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer'
+};
+transport.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+});
